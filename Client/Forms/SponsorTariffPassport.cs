f77ab5b956c2b9cc3c64dbd7e4d0ffd42d8e0d97
MikeFog <mfogilev@fogsoft.ru>
2026-03-28 15:13:10 -0300

    Ошибка создания и редактирования тарифа для спонсорской программы - эхо от удаления чек-бокса на странице со свойствами

--- Client/Forms/SponsorTariffPassport.cs
+++ Client/Forms/SponsorTariffPassport.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Diagnostics;
 using System.Windows.Forms;
@@ -10,9 +11,8 @@ namespace Merlin.Forms
 	public partial class SponsorTariffPassport : PassportForm
 	{
 		private CheckBox chkIsAlive;
-		private const int controlsNum = 5;
-		private readonly Control[] dependentsControls = new Control[controlsNum];
-		private readonly string[] strControlsIDs = new string[controlsNum] { "path", "suffix", "needInJingle", "needOutJingle", "needExt" };
+		private readonly IList<Control> _dependentsControls = new List<Control>();
+		private readonly string[] _strControlsIDs = new string[] { "path", "suffix", "needInJingle", "needOutJingle" };
 
 		public SponsorTariffPassport()
 		{
@@ -32,8 +32,8 @@ namespace Merlin.Forms
 			chkIsAlive = FindControl("isAlive") as CheckBox;
 			chkIsAlive.CheckedChanged += chkIsAlive_CheckedChanged;
 
-			for (int i = 0; i < controlsNum; i++)
-				dependentsControls[i] = FindControl(strControlsIDs[i]);
+			for (int i = 0; i < _strControlsIDs.Length; i++)
+				_dependentsControls.Add(FindControl(_strControlsIDs[i]));
 
 			updateDependentsFields();
 		}
@@ -45,8 +45,8 @@ namespace Merlin.Forms
 
 		private void updateDependentsFields()
 		{
-			for (int i = 0; i < controlsNum; i++)
-				dependentsControls[i].Enabled = !chkIsAlive.Checked;
+			for (int i = 0; i < _dependentsControls.Count; i++)
+				_dependentsControls[i].Enabled = !chkIsAlive.Checked;
 		}
 	}
 }
