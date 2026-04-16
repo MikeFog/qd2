7988bd23f185b9424e559183211ec804368d71aa
MikeFog <mfogilev@fogsoft.ru>
2026-04-15 21:27:18 -0300

    Прайм - максимум в дне

---
+++ ArtvisDB/dbo/Stored Procedures/GetMaxPriceByDay.sql
@@ -0,0 +1,18 @@
+﻿
+CREATE   PROCEDURE [dbo].[GetMaxPriceByDay]
+    @MassMediaId SMALLINT,
+    @DateFrom    DATE,
+    @DateTo      DATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT
+        CAST(tw.dayActual AS DATE) AS [Date],
+        MAX(tw.price) AS [Price]
+    FROM dbo.TariffWindow tw
+    WHERE tw.massmediaID = @MassMediaId
+      AND tw.dayActual >= @DateFrom
+      AND tw.dayActual < DATEADD(DAY, 1, @DateTo)
+    GROUP BY CAST(tw.dayActual AS DATE)
+END
