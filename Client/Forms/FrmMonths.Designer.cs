6168e917c948929a4e6bfa84eeacab62364a12b1
MikeFog <mfogilev@fogsoft.ru>
2026-06-23 19:16:35 -0300

    Необходимо модифицировать дизайн формы FrmMonths.

--- Client/Forms/FrmMonths.Designer.cs
+++ Client/Forms/FrmMonths.Designer.cs
@@ -28,73 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
-			this.btnCancel = new System.Windows.Forms.Button();
-			this.btnOk = new System.Windows.Forms.Button();
-			this.lstMonth = new System.Windows.Forms.CheckedListBox();
-			this.chkOption = new System.Windows.Forms.CheckBox();
-			this.SuspendLayout();
-			// 
-			// btnCancel
-			// 
-			this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.System;
-			this.btnCancel.Location = new System.Drawing.Point(144, 243);
-			this.btnCancel.Name = "btnCancel";
-			this.btnCancel.Size = new System.Drawing.Size(75, 23);
-			this.btnCancel.TabIndex = 11;
-			this.btnCancel.Text = "Отмена";
-			// 
-			// btnOk
-			// 
-			this.btnOk.DialogResult = System.Windows.Forms.DialogResult.OK;
-			this.btnOk.FlatStyle = System.Windows.Forms.FlatStyle.System;
-			this.btnOk.Location = new System.Drawing.Point(64, 243);
-			this.btnOk.Name = "btnOk";
-			this.btnOk.Size = new System.Drawing.Size(75, 23);
-			this.btnOk.TabIndex = 10;
-			this.btnOk.Text = "Ок";
-			// 
-			// lstMonth
-			// 
-			this.lstMonth.CheckOnClick = true;
-			this.lstMonth.FormattingEnabled = true;
-			this.lstMonth.Location = new System.Drawing.Point(13, 13);
-			this.lstMonth.Name = "lstMonth";
-			this.lstMonth.Size = new System.Drawing.Size(270, 196);
-			this.lstMonth.TabIndex = 12;
-			// 
-			// chkOption
-			// 
-			this.chkOption.AutoSize = true;
-			this.chkOption.Location = new System.Drawing.Point(13, 215);
-			this.chkOption.Name = "chkOption";
-			this.chkOption.Size = new System.Drawing.Size(104, 17);
-			this.chkOption.TabIndex = 13;
-			this.chkOption.Text = "Включать цену";
-			this.chkOption.UseVisualStyleBackColor = true;
-			// 
-			// FrmMonths
-			// 
-			this.AcceptButton = this.btnOk;
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.CancelButton = this.btnCancel;
-			this.ClientSize = new System.Drawing.Size(296, 281);
-			this.Controls.Add(this.chkOption);
-			this.Controls.Add(this.lstMonth);
-			this.Controls.Add(this.btnCancel);
-			this.Controls.Add(this.btnOk);
-			this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-			this.MaximizeBox = false;
-			this.MinimizeBox = false;
-			this.Name = "FrmMonths";
-			this.Padding = new System.Windows.Forms.Padding(10);
-			this.ShowIcon = false;
-			this.ShowInTaskbar = false;
-			this.Text = "Выбор месяца";
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.lstMonth = new System.Windows.Forms.CheckedListBox();
+            this.chkOption = new System.Windows.Forms.CheckBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnCancel.Location = new System.Drawing.Point(237, 3);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(111, 38);
+            this.btnCancel.TabIndex = 11;
+            this.btnCancel.Text = "Отмена";
+            // 
+            // btnOk
+            // 
+            this.btnOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOk.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnOk.Location = new System.Drawing.Point(120, 3);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(111, 38);
+            this.btnOk.TabIndex = 10;
+            this.btnOk.Text = "Ок";
+            // 
+            // lstMonth
+            // 
+            this.lstMonth.CheckOnClick = true;
+            this.lstMonth.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstMonth.FormattingEnabled = true;
+            this.lstMonth.Location = new System.Drawing.Point(4, 5);
+            this.lstMonth.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.lstMonth.Name = "lstMonth";
+            this.lstMonth.Size = new System.Drawing.Size(351, 302);
+            this.lstMonth.TabIndex = 12;
+            // 
+            // chkOption
+            // 
+            this.chkOption.AutoSize = true;
+            this.chkOption.Location = new System.Drawing.Point(4, 317);
+            this.chkOption.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.chkOption.Name = "chkOption";
+            this.chkOption.Size = new System.Drawing.Size(158, 28);
+            this.chkOption.TabIndex = 13;
+            this.chkOption.Text = "Включать цену";
+            this.chkOption.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.lstMonth, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.chkOption, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(11, 11);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(359, 404);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.Controls.Add(this.btnCancel);
+            this.flowLayoutPanel1.Controls.Add(this.btnOk);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(4, 355);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(351, 44);
+            this.flowLayoutPanel1.TabIndex = 14;
+            // 
+            // FrmMonths
+            // 
+            this.AcceptButton = this.btnOk;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(381, 426);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Font = new System.Drawing.Font("Segoe UI Variable Text", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmMonths";
+            this.Padding = new System.Windows.Forms.Padding(11, 11, 11, 11);
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Выбор месяца";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
 
         }
 
@@ -104,5 +141,8 @@
         private System.Windows.Forms.Button btnOk;
         private System.Windows.Forms.CheckedListBox lstMonth;
         private System.Windows.Forms.CheckBox chkOption;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
     }
 }
+
